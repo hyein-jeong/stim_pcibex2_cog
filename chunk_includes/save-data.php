a1ae2c1fcfc145a8c17cf3a8c6cdc861f02de47b
Jeong <hyeinjeong1010@gmail.com>
2020-09-14 16:02:32 +0200

    update js files

---
+++ chunk_includes/save-data.php
@@ -0,0 +1,11 @@
+<?php
+    // the $_POST[] array will contain the passed in filename and data
+    // the directory "data" is writable by the server (chmod 777)
+    // the file path needs to be adjusted accordingly!
+    // $filename = "data/".$_POST['filename'];
+    // $data = $_POST['fileName'];
+    // write the file to disk
+    file_put_contents($filename, $data);    
+?>
+
+
